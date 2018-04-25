961fbdeac1cc32e7706f8536eb7fa87d8e699f86
Viktoria Chebotar <viktoria_chebotar@epam.com>
2018-04-25 17:58:44 +0300

    Add keys, fix bug with home page loading

--- data/src/main/java/com/mydictionary/data/wordlistrepo/WordListRepositoryImpl.kt
+++ data/src/main/java/com/mydictionary/data/wordlistrepo/WordListRepositoryImpl.kt
@@ -2,7 +2,6 @@ package com.mydictionary.data.wordlistrepo
 
 import com.mydictionary.data.wordlistrepo.datasource.cache.WordListCache
 import com.mydictionary.data.wordlistrepo.datasource.remote.WordListDataSource
-import com.mydictionary.data.wordlistrepo.pojo.WordListDto
 import com.mydictionary.domain.entity.WordList
 import com.mydictionary.domain.repository.WordListRepository
 import io.reactivex.Single
@@ -17,8 +16,9 @@ class WordListRepositoryImpl @Inject constructor(
 ) : WordListRepository {
 
     override fun getAllWordLists(): Single<List<WordList>> = Single
-        .create<List<WordListDto>> { emitter ->
-            if (cache.get()?.isNotEmpty() == true) emitter.onSuccess(cache.get()!!)
+        .just("")
+        .flatMap {
+            if (cache.get()?.isNotEmpty() == true) Single.just(cache.get()!!)
             else remoteWordListDataSource.getAllWordLists()
         }
         .doOnEvent { t1, t2 ->
@@ -27,11 +27,11 @@ class WordListRepositoryImpl @Inject constructor(
         .map { mapper.mapListOfWordList(it) }
 
 
-    override fun getWordList(wordListName: String): Single<WordList> = Single
-        .create<WordListDto> { emitter ->
+    override fun getWordList(wordListName: String): Single<WordList> = Single.just("")
+        .flatMap {
             if (cache.get()?.isNotEmpty() == true) {
                 cache.get()?.find { it.name == wordListName }?.let {
-                    emitter.onSuccess(it)
+                    Single.just(it)
                 }
             } else remoteWordListDataSource.getWordList(wordListName)
         }
